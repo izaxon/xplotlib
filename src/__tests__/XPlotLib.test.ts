548eb4ae066184a009c6d7ae90a5f1bd12d11c9b
Johan Isaksson <johan@izaxon.com>
2022-06-17 15:15:27 +0200

    some code in place

---
+++ src/__tests__/XPlotLib.test.ts
@@ -0,0 +1,6 @@
+import { XPlotLib } from '../index';
+test('XPlotLib', () => {
+  // indexedDB is not available in Node, but should be available in the browser...
+  //   const lib = new XPlotLib();
+  //   lib.add({ timestamp: Date.now(), value: 123 });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,65 @@
 export const Hello = (name: string) => `Hello ${name}`;
+
+class IndexedDB {
+  private static connect = (
+    database: string,
+    upgrade: ((this: IDBOpenDBRequest, ev: IDBVersionChangeEvent) => any) | null,
+    version?: number,
+  ) => {
+    return new Promise<Event>((resolve: (e: Event) => void, reject: (e: Event) => void) => {
+      const request = indexedDB.open(database, version);
+
+      request.onsuccess = (e: Event) => {
+        resolve(e);
+      };
+
+      request.onerror = (e) => {
+        reject(e);
+      };
+
+      request.onupgradeneeded = upgrade;
+    });
+  };
+
+  static create = (database: string, table: string, version?: number) => {
+    return new Promise((resolve, reject) => {
+      (async () => {
+        IndexedDB.connect(
+          database,
+          (e) => {
+            //@ts-ignore
+            let db = e.target.result;
+            const _store = db.createObjectStore(table, { keyPath: 'id', autoIncrement: true });
+            resolve(db);
+          },
+          version,
+        )
+          .then(resolve)
+          .catch(reject);
+      })();
+    });
+  };
+
+  static add = (db: any, table: string, item: any) => {
+    db.transaction([table], 'readwrite').objectStore(table).add(item).onsuccess = () => {};
+  };
+
+  //   db.transaction(['todo'], 'readonly')
+  //   .objectStore('todo')
+  //   .get(1)
+  //   .onsuccess = data => console.log( data.target.result );
+}
+
+export class XPlotLib {
+  private db: any;
+
+  constructor() {
+    IndexedDB.create('xplot', 'data', 1)
+      .then((db) => (this.db = db))
+      .catch((e) => console.warn(e));
+  }
+
+  add(data: any) {
+    IndexedDB.add(this.db, 'data', data);
+  }
+}
